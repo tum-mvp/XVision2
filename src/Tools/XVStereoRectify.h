a373cb296ae9255b2da4cdb1075e6b188e829f3d
burschka <burschka@b6ea216b-604f-0410-98bb-8b453d65e082>
2010-09-19 11:24:59 +0000

    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/xvision2/Trunk/XVision2@142 b6ea216b-604f-0410-98bb-8b453d65e082

--- src/Tools/XVStereoRectify.h
+++ src/Tools/XVStereoRectify.h
@@ -27,8 +27,8 @@ class XVStereoRectify
      Config		   config;
      XVMatrix 	           K_ideal;
      XVColVector           cross(XVColVector &v1,XVColVector &v2);
-   public:
      XVImageScalar<u_char> temp_image1,temp_image2;
+   public:
      XVImageScalar<u_char> gray_image_l,gray_image_r;
      XVImageScalar<float>  calc_disparity(XVImageScalar<u_char> &image_l,
      					  XVImageScalar<u_char> &image_r,
